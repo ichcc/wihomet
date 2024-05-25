070782ce05851769bcca998565572f9ff6ef6295
Volodymyr Brodsky <ichcc@me.com>
2024-05-25 15:43:21 +0300

    Update main.tf from resources to modules

--- infra/main.tf
+++ infra/main.tf
@@ -3,6 +3,24 @@ provider "aws" {
   region = "us-east-1"
 }
 
+locals {
+  cluster_name = "wihomet-eks-${random_string.suffix.result}"
+}
+
+resource "random_string" "suffix" {
+  length  = 8
+  special = false
+}
+
+
+# Filter out local zones, which are not currently supported 
+# with managed node groups
+data "aws_availability_zones" "available" {
+  filter {
+    name   = "opt-in-status"
+    values = ["opt-in-not-required"]
+  }
+}
 
 terraform {
   backend "s3" {
@@ -10,109 +28,105 @@ terraform {
     key    = "wihoemt/terraform.tfstate"
     region = "us-east-1"
     dynamodb_table = "wihomet-lock-table"  
-    # Uncomment these lines if you need to access the bucket through a custom endpoint
-    # endpoint        = "s3.custom.endpoint"
-    # skip_credentials_validation = true
-    # skip_metadata_api_check     = true
-    # force_path_style             = true
   }
 }
 
 
 
-# Define VPC
-resource "aws_vpc" "wihoemt-vpc" {
-    cidr_block = "10.0.0.0/16"
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.8.1"
+
+  name = "wihoemt-vpc"
+
+  cidr = "10.0.0.0/16"
+  azs  = slice(data.aws_availability_zones.available.names, 0, 3)
+
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = 1
+  }
 }
 
-# Define Subnets
-resource "aws_subnet" "wihoemt-subnet-1" {
-    vpc_id                  = aws_vpc.wihoemt-vpc.id
-    cidr_block              = "10.0.1.0/24"
-    availability_zone       = "us-east-1a"
-}
-resource "aws_subnet" "wihoemt-subnet-2" {
-    vpc_id                  = aws_vpc.wihoemt-vpc.id
-    cidr_block              = "10.0.3.0/24"
-    availability_zone       = "us-east-1b"
-}
 
-# Define Security Groups
-resource "aws_security_group" "wihoemt-security-group" {
-    name        = "wihoemt-security-group"
-    description = "Allow HTTP inbound traffic"
-    vpc_id      = aws_vpc.wihoemt-vpc.id
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.8.5"
 
-    # Define ingress rules
-    ingress {
-        from_port   = 80
-        to_port     = 80
-        protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
+  cluster_name    = local.cluster_name
+  cluster_version = "1.29"
+
+  cluster_endpoint_public_access           = true
+  enable_cluster_creator_admin_permissions = true
+
+  cluster_addons = {
+    aws-ebs-csi-driver = {
+      service_account_role_arn = module.irsa-ebs-csi.iam_role_arn
+    }
+  }
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  eks_managed_node_group_defaults = {
+    ami_type = "AL2_x86_64"
+
+  }
+
+  eks_managed_node_groups = {
+    one = {
+      name = "node-group-1"
+
+      instance_types = ["t3.small"]
+
+      min_size     = 1
+      max_size     = 3
+      desired_size = 2
     }
 
-    # Define egress rules
-    egress {
-        from_port   = 0
-        to_port     = 0
-        protocol    = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
+    two = {
+      name = "node-group-2"
+
+      instance_types = ["t3.small"]
+
+      min_size     = 1
+      max_size     = 2
+      desired_size = 1
     }
-}
-
-# Define Routing Tables
-resource "aws_route_table" "wihoemt-route-table" {
-    vpc_id = aws_vpc.wihoemt-vpc.id
+  }
 }
 
 
-# Define IAM Role
-resource "aws_iam_role" "wihoemt_eks_role" {
-    name = "wihomet-eks-role"
-    assume_role_policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Principal": {
-                "Service": ["eks.amazonaws.com", "ec2.amazonaws.com"]
-            },
-            "Action": "sts:AssumeRole"
-        }
-    ]
-}
-EOF
+# https://aws.amazon.com/blogs/containers/amazon-ebs-csi-driver-is-now-generally-available-in-amazon-eks-add-ons/ 
+data "aws_iam_policy" "ebs_csi_policy" {
+  arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
 }
 
-# Define EKS Cluster
-resource "aws_eks_cluster" "wihomet-eks-cluster" {
-    name     = "wihoemt-eks-cluster"
-    role_arn = aws_iam_role.wihoemt_eks_role.arn
-    version  = "1.29"
-    vpc_config {
-        subnet_ids = [aws_subnet.wihoemt-subnet-1.id, aws_subnet.wihoemt-subnet-2.id]
-    }
+module "irsa-ebs-csi" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
+  version = "5.39.0"
+
+  create_role                   = true
+  role_name                     = "AmazonEKSTFEBSCSIRole-${module.eks.cluster_name}"
+  provider_url                  = module.eks.oidc_provider
+  role_policy_arns              = [data.aws_iam_policy.ebs_csi_policy.arn]
+  oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
 }
 
-resource "aws_eks_node_group" "wihomet_eks_node_group" {
-    cluster_name    = aws_eks_cluster.wihomet-eks-cluster.name
-    node_group_name = "wihomet-eks-node-group"
-    node_role_arn   = aws_iam_role.wihoemt_eks_role.arn
-    subnet_ids      = [aws_subnet.wihoemt-subnet-1.id, aws_subnet.wihoemt-subnet-2.id]
 
-    scaling_config {
-        desired_size = 1
-        max_size     = 1
-        min_size     = 1
-    }
 
-    instance_types = ["t3.medium"]
 
-    depends_on = [
-        aws_eks_cluster.wihomet-eks-cluster,
-    ]
-}
 
 # Define ECR Repository
 resource "aws_ecr_repository" "wihomet-ecr-repo" {
